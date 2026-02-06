8be22410db7f156087f40ad09964e6ef841cd095
Debjani <123620644+Debjani-99@users.noreply.github.com>
2026-02-06 11:45:42 -0500

    init dashboard protected page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,24 @@
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  // This is TEMP: it will allow UI development without auth/db
+  // Will remove this when NextAuth is ready
+  const devBypass = process.env.NODE_ENV === "development";
+
+  if (!devBypass) {
+    // When auth is ready, need to replace this with real session check
+    redirect("/login");
+  }
+
+  return (
+  <main className="min-h-screen flex items-center justify-center bg-gray-50">
+    <div className="p-8 rounded-lg shadow-md bg-white text-center">
+      <h1 className="text-8xl font-bold mb-2 text-blue-300">Dashboard</h1>
+      <p className=" text-2xl text-gray-500">
+        Welcome, User
+      </p>
+    </div>
+  </main>
+);
+
+}
